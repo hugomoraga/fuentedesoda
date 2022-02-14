453598c91f90fb6eb4e863224673cee47aaddf95
Hugo M <hg.moraga@gmail.com>
2022-02-14 20:24:48 -0300

    ADD: Setup: Webpack Postcss Tailwindcss

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = (config) => {
+  config.addPlugin(syntaxHighlight);
+  config.addPassthroughCopy({ "src/assets": "assets" });
+  config.setFrontMatterParsingOptions({
+    excerpt: true,
+    excerpt_separator: "<!-- excerpt -->",
+  });
+  config.setTemplateFormats(["jpg", "png", "webp", "md", "njk"]);
+  config.setBrowserSyncConfig({
+    files: ["dist/**/*"],
+    open: true,
+  });
+  config.setDataDeepMerge(true);
+
+  return {
+    pathPrefix: "/fuentedesoda",
+
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
